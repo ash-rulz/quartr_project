2af3047073ab6e2b71c81606f1f00a3947c25402
Mandakath Gopinath, Aswath <AMANDAKA@volvocars.com>
2026-03-28 23:12:41 +0100

    Environment variable setting

---
+++ main.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+if __name__ == "__main__":
+    load_dotenv()
+    user_agent = os.getenv("SEC_USER_AGENT")
+    if not user_agent:
+        raise ValueError("SEC_USER_AGENT environment variable is not set.")
+    print(f"Using SEC_USER_AGENT: {user_agent}")
